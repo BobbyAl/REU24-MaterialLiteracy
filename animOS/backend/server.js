e92b5720bd63c97c5b21df4e20b23444e47b5e46
Bobby Flennoy <87404756+BobbyAl@users.noreply.github.com>
2024-06-29 20:14:53 +0000

    connected backend

--- animOS/backend/server.js
+++ animOS/backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to animOS API" });
+app.get("/api", (req, res) => {
+    res.json({ "users": ["userOne", "userTwo", "userThree"] });
 })
 
 app.listen(5000, () => {console.log("Server is running on port 5000") })
--- animOS/frontend/src/App.js
+++ animOS/frontend/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      resonpse => resonpse.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
     <div>
-      
+      {(typeof backendData.users === 'undefined') ? (
+        <p>'Loading...'</p>
+       ) : (
+        backendData.users.map((users, i) => (
+          <p key={i}>{users}</p>
+        ))
+      )}
     </div>
   )
 }
